b88fa6c9dbd73d9463c77aeec65ab5567b8ad316
7700560596 <luan.silva-ext@viavarejo.com.br>
2022-08-27 21:59:37 -0300

    Adicionando print de teste

--- src/main/java/br/com/shoppinglist/Main.java
+++ src/main/java/br/com/shoppinglist/Main.java
@@ -3,5 +3,7 @@ package main.java.br.com.shoppinglist;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Start !!");
+
+        System.out.println(" TESTE GIT");
     }
 }
