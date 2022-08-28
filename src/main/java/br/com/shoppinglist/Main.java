a86ef0d184abbe11d5d66387d5af4ef932a09d3a
7700560596 <luan.silva-ext@viavarejo.com.br>
2022-08-27 22:18:38 -0300

    Implementando pacotes estrutura do projeto

--- src/main/java/br/com/shoppinglist/Main.java
+++ src/main/java/br/com/shoppinglist/Main.java
@@ -3,7 +3,5 @@ package main.java.br.com.shoppinglist;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Start !!");
-
-        System.out.println(" TESTE GIT");
     }
 }
